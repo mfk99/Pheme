cbb88404aa0406fb450433e7e5746ecda0fb2cea
Matti Kähkönen <matti.kahkonen99@gmail.com>
2026-02-17 10:00:18 +0200

    express server hosting

--- server/index.js
+++ server/index.js
@@ -1,14 +1,12 @@
-import * as http from "http";
+import express from "express";
 
-const hostname = "127.0.0.1";
-const port = 3000;
+const app = express();
+const port = process.env.PORT || 4000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World");
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
